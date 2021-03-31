84ff79bcd39ee809d5f493eb072a4352b2b5c9d9
Judit Mendez Puig <mendez.puig@gmail.com>
2021-03-31 18:15:36 +0000

    add http_basic_authenticate for username & password

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,16 @@
 class Admin::DashboardController < ApplicationController
+  # http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'] 
+  http_basic_authenticate_with(
+      name: ENV['ADMIN_USERNAME'],
+      password: ENV['ADMIN_PASSWORD']
+    )
   def show
+    # Display a count of how many products are in the database
+    @products = Product.all
+    @products_count= @products.count
+
+    # Display a count of how many categories are in the database
+    @category = Category.all
+    @category_count= @category.count
   end
 end

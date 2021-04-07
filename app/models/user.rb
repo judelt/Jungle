b7866a01369ae9a9b0276ddac583716b40d61378
Judit Mendez Puig <mendez.puig@gmail.com>
2021-04-07 03:40:33 +0000

    fix bug & refactor with terniary operator

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,12 +13,8 @@ class User < ActiveRecord::Base
   def self.authenticate_with_credentials(email, password)
     email = email.strip.downcase
     user = User.find_by_email(email)
-
-    if user.authenticate(password)
-      user
-    else
-      nil
-    end
+    user && user.authenticate(password) ? user : nil
+   
   end
 
 end
